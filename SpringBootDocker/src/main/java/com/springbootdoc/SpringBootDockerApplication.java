bccdd70b84ed0eb6f4e42d4dd00d42c5d9e29548
Shreyanshi Shukla <89104546+ShreyanshiShukla0920@users.noreply.github.com>
2023-02-06 12:03:44 +0530

    Add files via upload

---
+++ SpringBootDocker/src/main/java/com/springbootdoc/SpringBootDockerApplication.java
@@ -0,0 +1,17 @@
+package com.springbootdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+	
+	
+
+}
---
+++ SpringBootDocker/src/main/java/com/springbootdoc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.springbootdoc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String message() {
+		return "Docker Springboot";
+	}
+
+}
